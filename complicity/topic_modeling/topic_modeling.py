e57954ec1ca205caac745e0b1eba4e4e3555dfed
Brian Abelson <brianabelson@gmail.com>
2013-11-09 10:29:23 +0100

    added pickling to topic modeling

--- complicity/topic_modeling/topic_modeling.py
+++ complicity/topic_modeling/topic_modeling.py
@@ -17,40 +17,49 @@ def train_model(texts, **kwargs):
   use_pickle = kwargs.get('use_pickle', True)
   update_pickle = kwargs.get('update_pickle', True)
   report = kwargs.get('report', True)
+  distributed = kwargs.get('distributed', False)
   
   # build corpus or read it in from pickle
   if use_pickle:
-    print "loading pickled corpus and word hash"
-    corpus = pickle.load( open( "corpus.p", "rb" ) )
-    id2word = corpora.Dictionary(pickle.load( open( "id2word.p", "rb" ) ))
+    print "INFO: loading pickled corpus and word hash"
+    corpus = pickle.load( open( "pickles/corpus.p", "rb" ) )
+    id2word = pickle.load( open( "pickles/id2word.p", "rb" ) )
             
   else:
-    print "processing text and building corpus..."
+    print "INFO: processing text and building corpus..."
     corpus, id2word = process_texts(
       texts = texts, 
       filter_stopwords = filter_stopwords,
       normalizer = normalizer,
       min_freq = min_freq
     )
+
     if update_pickle:
       # pickle files
-      print "updating pickled coprus and word hash"
+      print "INFO: updating pickled coprus and word hash"
       pickle.dump(corpus, open( "pickles/corpus.p", "wb" ) )
-      pickle.dump(corpus, open( "pickles/id2word.p", "wb" ) )
+      pickle.dump(id2word, open( "pickles/id2word.p", "wb" ) )
 
   # optional tfidf transformation
   if tfidf:
-    print "applying tfidf transformation..."
+    print "INFO: applying tfidf transformation..."
     tfidf = TfidfModel(corpus)
     corpus = tfidf[corpus]
 
   # fit model
-  print "fitting model..."
-  lda = LdaModel(corpus = corpus, id2word = id2word, num_topics = num_topics)
+  print "INFO: fitting model..."
+  lda = LdaModel(
+    corpus = corpus, 
+    id2word = id2word, 
+    num_topics = num_topics,
+    distributed = distributed
+  )
 
   # report
   if report:
-    print "\nperplexity: ", lda.bound(corpus)
+    perplexity = lda.bound(corpus)
+    print "RESULTS:"
+    print "\nperplexity: ", perplexity, "\n"
     topics = lda.show_topics(num_topics)
     for i, t in enumerate(topics):
       print "topic %d:" % i
@@ -64,12 +73,11 @@ if __name__ == '__main__':
   # texts = [a['content'] for a in nyt_articles]
 
   # real data
-  texts = [r['content'] for r in db.query('SELECT content FROM articles')][1:100]
+  texts = [r['content'] for r in db.query('SELECT content FROM articles')]
     
   train_model(
     texts = texts, 
-    num_topics = 10,
-    min_freq = 2,
-    use_pickle = False,
-    update_pickle = False
+    num_topics = 40,
+    min_freq = 5,
+    use_pickle = True
   )
